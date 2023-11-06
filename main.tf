e95b2f50bcdca6bd9bf2e628f83f235aed92d0c7
Rafael de Miranda <37124918+rafaeldemiranda95@users.noreply.github.com>
2023-11-05 22:09:59 -0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ provider "google" {
 }
 
 resource "google_sql_database_instance" "postgres_instance" {
-  name             = "postgres-instance"
+  name             = "teste"
   database_version = "POSTGRES_15"
   region           = var.region
 
