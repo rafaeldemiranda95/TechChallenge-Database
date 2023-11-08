3e53eac8b34a22e5ceca8c1901339bc46cae8400
Rafael de Miranda <37124918+rafaeldemiranda95@users.noreply.github.com>
2023-11-08 20:06:10 -0300

    Teste

--- sql/check_and_run_sql.sh
+++ sql/check_and_run_sql.sh
@@ -1,23 +1,15 @@
-#!/bin/bash
-# check_and_run_sql.sh
-# Uso: sh check_and_run_sql.sh [CONNECTION_NAME] [USER] [PASSWORD] [DATABASE_NAME]
-
 CONNECTION_NAME=$1
 USER=$2
 PASSWORD=$3
 DATABASE_NAME=$4
 PATH_TO_SQL_SCRIPT="sql/init.sql"
 
-# Expor a senha para o psql não pedir interativamente
 export PGPASSWORD=$PASSWORD
 
-# Obter a contagem de tabelas no schema 'public'
 TABLE_COUNT=$(psql -h $CONNECTION_NAME -U $USER -d $DATABASE_NAME -t -c "SELECT COUNT(*) FROM information_schema.tables WHERE table_schema = 'public';")
 
-# Remover espaços em branco do resultado
 TABLE_COUNT=$(echo $TABLE_COUNT | xargs)
 
-# Verificar se o banco de dados está vazio e executar o script SQL
 if [ "$TABLE_COUNT" -eq 0 ]; then
   echo "O banco de dados está vazio. Iniciando o script SQL..."
   psql -h $CONNECTION_NAME -U $USER -d $DATABASE_NAME -a -f $PATH_TO_SQL_SCRIPT
@@ -25,5 +17,4 @@ else
   echo "O banco de dados já possui tabelas. Nenhuma ação foi realizada."
 fi
 
-# Limpar a variável de ambiente
 unset PGPASSWORD
