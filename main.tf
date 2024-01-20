7bdfe5612f7a3fad78b97463e71b0dadc57c639d
Rafael de Miranda <37124918+rafaeldemiranda95@users.noreply.github.com>
2024-01-20 13:11:41 -0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,17 +3,9 @@ provider "google" {
   region  = var.region
 }
 
-# resource "google_sql_database_instance" "postgres_instance" {
-#   name             = "tech-challenge-db"
-#   database_version = "POSTGRES_15"
-#   region           = var.region
-
-#   settings {
-#     tier = "db-f1-micro"
-#   }
-# }
+# Instâncias do Banco de Dados
 resource "google_sql_database_instance" "techchallenge_pagamento" {
-  name             = "techchallenge_pagamento"
+  name             = "techchallenge-pagamento" # Nome corrigido
   database_version = "POSTGRES_15"
   region           = var.region
 
@@ -21,8 +13,9 @@ resource "google_sql_database_instance" "techchallenge_pagamento" {
     tier = "db-f1-micro"
   }
 }
+
 resource "google_sql_database_instance" "techchallenge_producao" {
-  name             = "techchallenge_producao"
+  name             = "techchallenge-producao" # Nome corrigido
   database_version = "POSTGRES_15"
   region           = var.region
 
@@ -30,8 +23,9 @@ resource "google_sql_database_instance" "techchallenge_producao" {
     tier = "db-f1-micro"
   }
 }
+
 resource "google_sql_database_instance" "techchallenge_pedido" {
-  name             = "techchallenge_pedido"
+  name             = "techchallenge-pedido" # Nome corrigido
   database_version = "POSTGRES_15"
   region           = var.region
 
@@ -40,59 +34,37 @@ resource "google_sql_database_instance" "techchallenge_pedido" {
   }
 }
 
-# Banco de dados principal
-# resource "google_sql_database" "default" {
-#   name     = "tech-challenge-db"
-#   instance = google_sql_database_instance.postgres_instance.name
-# }
-
-# Banco de dados para Pagamento
+# Bancos de Dados
 resource "google_sql_database" "techchallenge_pagamento" {
   name     = var.techchallenge_pagamento_db_name
   instance = google_sql_database_instance.techchallenge_pagamento.name
 }
 
-# Banco de dados para Produção
 resource "google_sql_database" "techchallenge_producao" {
   name     = var.techchallenge_producao_db_name
   instance = google_sql_database_instance.techchallenge_producao.name
 }
 
-# Banco de dados para Pedido
 resource "google_sql_database" "techchallenge_pedido" {
   name     = var.techchallenge_pedido_db_name
   instance = google_sql_database_instance.techchallenge_pedido.name
 }
 
-# resource "google_sql_user" "default" {
-#   name     = var.postgres_username
-#   instance = google_sql_database_instance.postgres_instance.name
-#   password = var.postgres_password
-# }
+# Usuários dos Bancos de Dados
 resource "google_sql_user" "techchallenge_pagamento" {
   name     = var.postgres_username
   instance = google_sql_database_instance.techchallenge_pagamento.name
   password = var.postgres_password
 }
+
 resource "google_sql_user" "techchallenge_producao" {
   name     = var.postgres_username
   instance = google_sql_database_instance.techchallenge_producao.name
   password = var.postgres_password
 }
+
 resource "google_sql_user" "techchallenge_pedido" {
   name     = var.postgres_username
   instance = google_sql_database_instance.techchallenge_pedido.name
   password = var.postgres_password
 }
-
-# resource "null_resource" "db_setup" {
-#   depends_on = [google_sql_database.default]
-
-#   # provisioner "local-exec" {
-#   #   command = "sh check_and_run_sql.sh ${google_sql_database_instance.postgres_instance.connection_name} ${var.postgres_password} ${google_sql_database.default.name}"
-#   # }
-
-#   triggers = {
-#     always_run = "${timestamp()}"
-#   }
-# }
